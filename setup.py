cdd1c6bfbe41b4d37247806f5253f045e1fe80b9
Kunal Bhutani <kunalbhutani@gmail.com>
2015-12-10 11:55:54 -0800

    Set it up as a package for ease-of-use going forward

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mediator_was',
+      version='0.1',
+      description='Mediator-wide association study toolbox',
+      url='http://github.com/Schork-Lab/mediator-was',
+      author='Kunal Bhutani, Abhishek Sarkar',
+      author_email='kbhutani@ucsd.edu',
+      license='MIT',
+      packages=['mediator_was'],
+      zip_safe=False)
