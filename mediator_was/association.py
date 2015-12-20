e826d6bec5a7a8385e5f7548d741ad615f483025
Abhishek Sarkar <aksarkar@mit.edu>
2015-12-19 22:14:00 -0500

    Implement RC for continuous phenotypes
    The method is exactly the same as for logistic regression (GLM), but the
    p-value comes from a t-test with 2 df.

--- mediator_was/association.py
+++ mediator_was/association.py
@@ -11,40 +11,65 @@ import statsmodels.api
 import statsmodels.tools
 
 # Freeze this for efficiency
-_sf = scipy.stats.chi2(1).sf
+_chi2 = scipy.stats.chi2(1).sf
+_t = scipy.stats.t(2).sf
 
-def _logit(expression, phenotype):
-    """Wrapper around statsmodels.api.Logit"""
-    design = statsmodels.tools.add_constant(expression)
-    model = statsmodels.api.Logit(phenotype, design)
-    fit = model.fit()
+def _wrap(model):
+    """Wrapper function around statsmodels API fit()"""
+    def fit(phenotype, expression):
+        assert phenotype.shape == expression.shape
+        design = statsmodels.tools.add_constant(expression)
+        return model(phenotype, design).fit()
     return fit
 
-def lrt(expression, phenotype, sigma_u):
-    """Return p-value of likelihood ratio test corrected by the reliability ratio.
+_ols = _wrap(statsmodels.api.OLS)
+_logit = _wrap(statsmodels.api.Logit)
 
-    expression - n x 1 array of predicted expression
-    phenotype - n x 1 array of phenotypes
-    sigma_u - scalar estimate of prediction error
+def reliability_ratio(expression, sigma_u):
+    """Return the reliability ratio given observed values and estimated error variance"""
+    sigma_x = numpy.var(expression) - sigma_u
+    return sigma_x / (sigma_x + sigma_u)
 
-    The naive slope estimate is off by a multiplicative factor (attenuation
-    bias), so rescale it in the alternative model.
+def t(expression, phenotype, sigma_u=None):
+    """Test for association of continuous phenotype to expression.
 
-    The bias is sigma_x / (sigma_x + sigma_u), and the correction is its
-    inverse.
+    If sigma_u is None, perform a naive analysis.
 
-    After correction, perform a nested model comparison dropping one variable
-    and compute tail probabilities from the chi-square distribution with one
-    degree of freedom.
+    If sigma_u is a scalar, rescale the naive estimate of the regression
+    coefficient by the reliability ratio.
+
+    If sigma_u is a vector, regress phenotype against imputed (true) expression
+    values.
 
     """
-    fit = _logit(expression, phenotype)
-    llnull = fit.llnull
-    sigma_x = numpy.var(expression)
-    fit.params[1] *= (sigma_x + sigma_u) / sigma_x
-    llalt = model.loglike(fit.params)
-    return _sf(-2 * (llnull - llalt))
+    fit = _ols(phenotype, expression)
+    assert(fit.params.shape[0] > 1)
+    if sigma_u is None:
+        return fit.pvalues[0]
+    elif numpy.isscalar(sigma_u):
+        return _t(fit.params[1] / reliability_ratio(expression, sigma_u))
+    else:
+        raise NotImplementedError
 
-def lrt_naive(expression, phenotype):
-    """Return naive p-value of likelihood ratio test."""
-    return _logit(expression, phenotype).llr_pvalue
+def lr(expression, phenotype, sigma_u=None):
+    """Test for association between binary phenotype and expression.
+
+    If sigma_u is None, perform a naive analysis.
+
+    If sigma_u is a scalar, rescale the naive estimate of the regression
+    coefficient by the reliability ratio.
+
+    If sigma_u is a vector, regress phenotype against imputed (true) expression
+    values.
+
+    """
+    fit = _logit(phenotype, expression)
+    if sigma_u is None:
+        return fit.llr_pvalue
+    elif numpy.isscalar(sigma_u):
+        llnull = fit.llnull
+        fit.params[1] /= reliability_ratio(expression, sigma_u)
+        llalt = model.loglike(fit.params)
+        return _chi2(-2 * (llnull - llalt))
+    else:
+        raise NotImplementedError
