125fd766098de1f2fb5add0ca1d9d01d43804ec7
Kunal Bhutani <kunalbhutani@gmail.com>
2016-04-08 09:51:44 -0700

    Committed changes to simulation, power, and association (plink genotype files).

--- mediator_was/power.py
+++ mediator_was/power.py
@@ -23,33 +23,45 @@ def trial(simulation, method, n_test=50000, alpha=0.05, ngwas_tests=1e6,
 
     genotypes, expression, phenotype = getattr(simulation, method)(n_test)
     associations = []
-    for g, e, ms, eqtl in zip(genotypes, expression, simulation.models, simulation.eqtls):
-        # predicted_expression = numpy.array([m.predict(g) for m in ms])
-        # predicted_expression = numpy.apply_along_axis(standardize, 1, predicted_expression)
-        # w = numpy.mean(predicted_expression, axis=0)
-        # sigma_ui = numpy.var(predicted_expression, axis=0)
+    for g, e, ms, bms, beta in zip(genotypes, expression, simulation.models, simulation.b_models,
+                              simulation.params.beta):#, simulation.eqtls):
         design = statsmodels.tools.add_constant(g)
+        predicted_expression = numpy.array([m.predict(design) for m in bms])
+        #predicted_expression = numpy.apply_along_axis(standardize, 1, predicted_expression)
+        w_inter = numpy.mean(predicted_expression, axis=0)
+        sigma_ui_inter = numpy.var(predicted_expression, ddof=1, axis=0)
         w = ms[0].predict(design)
-        sigma_ui = wls_prediction_std(ms[0], design)[0]
-        sigma_ui = numpy.power(sigma_ui, 2) - ms[0].mse_resid
+        sigma_ui =  (design * numpy.dot(ms[0].cov_params(), design.T).T).sum(1)
+        # sigma_ui = wls_prediction_std(ms[0], design)[0]
+        # sigma_ui = (numpy.power(sigma_ui, 2) - ms[0].mse_resid)
+        log_w = numpy.log(w)
+        log_sigma_ui = numpy.log(sigma_ui)
         sigma_u = numpy.mean(sigma_ui)
         association = {'OLS': t(w, phenotype, method="OLS"),
+                    'OLS-E': t(predicted_expression[0], phenotype, method="OLS"),
                    'WLS': t(w, phenotype, sigma_ui, method="WLS"),
-                   'Moment': t(w, phenotype, sigma_ui, method="moment"),
-                   'Moment2': t(w, phenotype, sigma_ui, method="moment2"),
-                   'Moment-Buo': t(w, phenotype, sigma_ui, method="momentbuo"),
-                   'RC': t(w, phenotype, sigma_ui, method="rc"),
-                   'RC-hetero': t(w, phenotype, sigma_ui, method="rc-hetero"),
+#                   'Moment': t(w, phenotype, sigma_ui, method="moment"),
+#                   'Moment2': t(w, phenotype, sigma_ui, method="moment2"),
+#                   'Moment-Buo': t(w, phenotype, sigma_ui, method="momentbuo"),
+#                   'RC': t(w, phenotype, sigma_ui, method="rc"),
+                    'RC-hetero-interstudy': t(w_inter, phenotype, sigma_ui_inter, method='rc-hetero'),
+                   'RC-hetero-intrastudy': t(w, phenotype, sigma_ui, method="rc-hetero"),
+                   'RC-hetero-log': t(log_w, phenotype, log_sigma_ui, method="rc-hetero"),
+                   'beta': (beta, beta, beta),
+                   'sigma_ui': (numpy.var(sigma_ui), numpy.var(sigma_ui_inter), numpy.mean(sigma_ui)),
+                   'w': (numpy.var(w), numpy.var(w_inter), numpy.mean(w)),
                    'Weighted': t(w, phenotype, sigma_ui, method="weighted")}
         associations.append(association)
     return associations
 
-def run_simulation(method, n_causal_eqtls, n_test, n_causal_genes=20, n_trials=25, hapgen=None):
+def run_simulation(method, n_causal_eqtls, n_test, n_causal_genes=100, n_trials=1, hapgen=None, plink=None):
     """Run the simulation with the specified parameters"""
     numpy.random.seed(0)
-    n_train = numpy.repeat(1000, 1)
+    n_train = numpy.repeat(350, 1)
+    bootstrap = (350, 350, 5)
     simulations = {}
-    with simulation(n_causal_eqtls, n_train, n_causal_genes, hapgen=hapgen) as sim:
+    with simulation(n_causal_eqtls, n_train, n_causal_genes, 
+                    bootstrap=bootstrap, hapgen=hapgen, plink=plink) as sim:
         for i in range(n_trials):
             associations = trial(sim, method, n_test=n_test)
             for gene, association in enumerate(associations):
@@ -63,19 +75,21 @@ def run_simulation(method, n_causal_eqtls, n_test, n_causal_genes=20, n_trials=2
     simulations_df['wald'] = simulations_df['coeff']**2 / simulations_df['se']**2
     return simulations_df
 
-def power(n_causal_eqtls, n_test, n_causal_genes, hapgen=None):
+def power(p_causal_eqtls, n_test, n_causal_genes, hapgen=None, plink=None):
     """Estimate the power of the naive and corrected analyses."""
-    return run_simulation("simulate_mediated_phenotype", n_causal_eqtls, n_test, n_causal_genes, hapgen=hapgen)
+    return run_simulation("simulate_mediated_phenotype", p_causal_eqtls, n_test, n_causal_genes, 
+                          hapgen=hapgen, plink=plink)
 
-def type_1_error_null(n_causal_eqtls, n_test, n_causal_genes, hapgen=None):
+def type_1_error_null(p_causal_eqtls, n_test, n_causal_genes, hapgen=None):
     """Estimate the type 1 error rate of the naive and corrected analyses assuming
 a null phenotype."""
-    return run_simulation("simulate_null_phenotype", n_causal_eqtls, n_test, n_causal_genes, hapgen=hapgen)
+    return run_simulation("simulate_null_phenotype", p_causal_eqtls, n_test, n_causal_genes, 
+                          hapgen=hapgen, plink=plink)
 
-def type_1_error_independent(n_causal_eqtls, n_test, n_causal_genes, hapgen=None):
+def type_1_error_independent(p_causal_eqtls, n_test, n_causal_genes, hapgen=None):
     """Estimate the type 1 error rate of the naive and corrected analyses assuming
 an independent phenotype."""
-    return run_simulation("simulate_independent_phenotype", n_causal_eqtls, n_test, n_causal_genes, hapgen=hapgen)
+    return run_simulation("simulate_independent_phenotype", p_causal_eqtls, n_test, n_causal_genes, hapgen=hapgen)
 
 if __name__ == '__main__':
     method = sys.argv[1]
--- mediator_was/simulation.py
+++ mediator_was/simulation.py
@@ -15,32 +15,31 @@ import numpy
 import numpy.random as R
 import sklearn.linear_model
 import sklearn.metrics
-import pandas
+import sklearn.utils
+
 
 from mediator_was.association import *
+from mediator_was.processing.helpers import load_hapgen, load_plink
+
 
 Gene = collections.namedtuple('Gene', ['maf', 'haps', 'beta', 'pve', 'pve_se'])
 Phenotype = collections.namedtuple('Phenotype', ['beta', 'genes', 'pve'])
 
 
-def _load_hapgen(hap_file):
-    """Load in a HapGen2 generated haplotypes file.
-    """
-    haps = pandas.read_table(hap_file, sep=" ", header=None)
-    return haps
 
-def _generate_gene_haps(haps, n_causal_eqtls, 
-                   min_maf=0.05, max_maf=0.5):
+def _sample_causal_loci(haps, n_causal_snps, min_maf=0.05, max_maf=0.5):
     """
-    Generate gene specific haplotypes based on a haplotype dataframe
+    Sample gene specific causal loci based on a haplotype dataframe
     restricted to a minor allele frequency range and total number
     of causal snps
     """
-    maf = haps.apply(lambda x: numpy.sum(x)/len(x), axis=1)
-    loci = numpy.where((maf > min_maf) & (maf < max_maf))[0]
-    loci = numpy.random.choice(loci, size=n_causal_eqtls)
-    haps = haps.ix[loci]
-    return haps
+    # maf = haps.apply(lambda x: numpy.sum(x)/len(x), axis=1)
+    # causal_loci = numpy.where((maf > min_maf) & (maf < max_maf))[0]
+    # if len(causal_loci) < n_causal_snps:
+    #     print('Warning: not enough causal loci in provided haplotypes', file=sys.stderr)
+    # causal_loci = R.choice(causal_loci, size=n_causal_snps)
+    causal_loci = R.choice(haps.index, size=n_causal_snps)
+    return causal_loci
 
 def _add_noise(genetic_value, pve):
     """Add Gaussian noise to genetic values to achieve desired PVE.
@@ -52,22 +51,31 @@ def _add_noise(genetic_value, pve):
     sigma = numpy.sqrt(numpy.var(genetic_value) * (1 / pve - 1))
     return genetic_value + R.normal(size=genetic_value.shape, scale=sigma)
 
-def _generate_gene_params(n_causal_eqtls, hap_df=None, pve=0.17, pve_se=0.05):
+def _generate_gene_params(p_causal_eqtls, hap_df=None, pve=0.17, pve_se=0.05, n_snps=80):
     """Return a matrix of haplotypes or a vector minor allele frequencies and a vector of effect sizes.
     The average PVE by cis-genotypes on gene expression is 0.17. Assume the
     average number of causal cis-eQTLs is 10.
 
     """
+    print(p_causal_eqtls)
     if hap_df is not None:
-        haps = _generate_gene_haps(hap_df, n_causal_eqtls)
+        haps = hap_df
         maf = None
+        n_snps = len(haps)
+        haps.index = range(n_snps)
+        n_causal_eqtls = numpy.round(n_snps*p_causal_eqtls)
+        causal_loci = _sample_causal_loci(haps, n_causal_eqtls)
     else:
         haps = None
-        maf = R.uniform(size=n_causal_eqtls, low=0.05, high=0.5)
-    beta = R.normal(size=n_causal_eqtls)
+        maf = R.uniform(size=n_snps, low=0.05, high=0.5)
+        n_causal_eqtls = numpy.round(n_snps*p_causal_eqtls)
+        causal_loci = R.random_integers(0, n_snps-1, size=n_causal_eqtls)
+
+    beta = numpy.zeros(n_snps)
+    beta[causal_loci] = R.normal(size=n_causal_eqtls)
     return Gene(maf, haps, beta, pve, pve_se)
 
-def _generate_phenotype_params(n_causal_genes=100, n_causal_eqtls=10, pve=0.2, hapgen=None):
+def _generate_phenotype_params(n_causal_genes=100, p_causal_eqtls=.1, pve=0.2, hapgen=None, plink=None):
     """Return causal gene effects and cis-eQTL parameters for causal genes.
 
     Each gene has an effect size sampled from N(0, 1). For each gene, generate
@@ -76,24 +84,34 @@ def _generate_phenotype_params(n_causal_genes=100, n_causal_eqtls=10, pve=0.2, h
     The PVE by high-effect loci associated to height is 0.2.
 
     """
-    beta = R.normal(size=n_causal_genes)
     if hapgen:
-        hap_df = _load_hapgen(hapgen)
+        n_causal_genes = len(hapgen)
+        genes = [_generate_gene_params(p_causal_eqtls, load_hapgen(gene)) 
+                 for gene in hapgen]
+    elif plink:
+        n_causal_genes = len(plink)
+        genes = [_generate_gene_params(p_causal_eqtls, load_plink(gene)) 
+                 for gene in plink]
     else:
-        hap_df = None
-    genes = [_generate_gene_params(n_causal_eqtls, hap_df) for _ in beta]
+        genes = [_generate_gene_params(p_causal_eqtls) 
+                 for _ in range(n_causal_genes)]  
+
+    beta = R.normal(size=n_causal_genes)
     return Phenotype(beta, genes, pve)
 
 def _simulate_gene(params, n=1000, center=True):
     """Return genotypes at cis-eQTLs and cis-heritable gene expression.
 
     n - number of individuals
+    same_cohort - boolean (default:False), choose the first n individuals
+            so that the training cohort remains the same.
 
     """
     if params.maf is not None:
         genotypes = R.binomial(2, params.maf, size=(n, params.maf.shape[0])).astype(float)
     else:
-        genotypes = params.haps[R.random_integers(low=0, high=params.haps.shape[1]-2, size=n)].T.values.astype(float)
+        genotypes = params.haps[R.random_integers(low=0, high=params.haps.shape[1]-2, size=n)]
+        genotypes = genotypes.T.values.astype(float)
     if center:
         genotypes -= numpy.mean(genotypes, axis=0)[numpy.newaxis,:]
     pve = R.normal(params.pve, params.pve_se)
@@ -103,31 +121,66 @@ def _simulate_gene(params, n=1000, center=True):
     return genotypes, expression
 
 class Simulation(object):
-    def __init__(self, n_causal_genes=100, n_causal_eqtls=10, n_train=None,
-                 hapgen=None):
+    def __init__(self, n_causal_genes=100, p_causal_eqtls=.1, 
+                 bootstrap=None, n_train=None,
+                 hapgen=None, plink=None, pve=0.2/50):
         """Initialize a new simulation.
 
-        There are two ways to generate the genotypes:
-
-        1. Use indepdent genotypes generated using a maf range of 0.05 to 0.5
-        2. Use hapgen2 generated haps file using the full path set using hapgen  
         
-        hapgen - path to the hapgen2 generated haps file. Default: None
+        There are two ways to train the G -> E models.
+
+        interstudy:
+        n_train - a numpy array specifying # of individuals in different studies
+        
+        intrastudy:
+        bootstrap - tuple (# samples in study, # samples per bootstrap, # bootstraps) 
+                     specifying models using bootstrapped samples from the same study. 
+
+        There are three ways to generate the genotypes:
+
+        1. Use independent genotypes generated using a maf range of 0.05 to 0.5
+        2. Use a separate hapgen2 genotype file for each gene  
+        3. Use a separate plink genotype file for each gene 
+
+        Note if hapgen or plink are set, the number of causal genes is
+        determined by the number of items in hapgen or plink lists.
+
+        hapgen - list of path to the hapgen2 generated haps file. Default: None
+        plink - list of paths to plink-prefix for bed files. Default: None
+
+
         """
-        R.seed(0)
+        #R.seed(0)
         if n_train is None:
             n_train = numpy.repeat(1000, 4)
         elif numpy.isscalar(n_train):
             n_train = numpy.array([n_train])
-        self.params = _generate_phenotype_params(n_causal_genes, n_causal_eqtls, hapgen=hapgen)
+        self.params = _generate_phenotype_params(n_causal_genes, p_causal_eqtls, 
+                                                 hapgen=hapgen, plink=plink, pve=pve)
         self.models = list(zip(*[self._train(n) for n in n_train]))
-        self.eqtls = self._eqtls(n_train[0])
+        if bootstrap:
+            self.b_models = self._b_train(bootstrap)
+
+        #self.eqtls = self._eqtls(n_train[0])
+
+    def _b_train(self, bootstrap):
+        b_models = []
+        for p in self.params.genes:
+            genotypes, expression = _simulate_gene(params=p, n=bootstrap[0])
+            gene_models = []
+            for i in range(bootstrap[2]):
+                b_genotypes, b_expression = sklearn.utils.resample(genotypes, expression, 
+                                                                   n_samples=bootstrap[1])
+                model = sklearn.linear_model.ElasticNetCV()
+                model.fit(b_genotypes, b_expression)
+                gene_models.append(model)
+            b_models.append(gene_models)
+        return b_models
 
     def _train(self, n=1000, model=statsmodels.api.OLS):
         """Train models on each cis-window.
 
         n - number of individuals
-        model - sklearn estimator
 
         """
         # models = [model() for _ in self.params.genes]
@@ -137,7 +190,9 @@ class Simulation(object):
         models = []
         for p in self.params.genes:
             genotypes, expression = _simulate_gene(params=p, n=n)
+            
             design = statsmodels.tools.add_constant(genotypes)
+            
             models.append(model(expression, design).fit())
 
         return models
@@ -147,7 +202,8 @@ class Simulation(object):
         eqtls = []
         for p, m in zip(self.params.genes, self.models):
             genotypes, expression = _simulate_gene(params=p, n=n)
-            eqtls.append(min((t(g, expression, method="OLS")[2], i) for i, g in enumerate(genotypes.T))[1])
+            eqtls.append(min((t(g, expression, method="OLS")[2], i) 
+                            for i, g in enumerate(genotypes.T))[1])
         return eqtls
 
     def _test(self, method, n=50000):
@@ -208,7 +264,7 @@ class Simulation(object):
             print('\t'.join('{:.3g}'.format(o) for o in outputs))
 
 @contextlib.contextmanager
-def simulation(n_causal_eqtls, n_train, n_causal_genes, hapgen=None):
+def simulation(p_causal_eqtls, n_train, n_causal_genes, bootstrap=None, hapgen=None, plink=None):
     """Retrieve a cached simulation, or run one if no cached simulation exists.
 
     Sample n_models training cohorts to learn linear models of gene expression.
@@ -218,14 +274,16 @@ def simulation(n_causal_eqtls, n_train, n_causal_genes, hapgen=None):
     against predicted expression.
 
     """
-    key = 'simulation-{}-{}-{}.pkl'.format(n_causal_eqtls, n_train, n_causal_genes)
+    key = 'simulation-{}-{}-{}.pkl'.format(p_causal_eqtls, n_train, n_causal_genes)
     hit = False
-    try:
-        with open(key, 'rb') as f:
-            sim = pickle.load(f)
-            hit = True
-    except:
-        sim = Simulation(n_causal_genes=n_causal_genes, n_causal_eqtls=n_causal_eqtls, n_train=n_train, hapgen=hapgen)
+    # try:
+    #     with open(key, 'rb') as f:
+    #         sim = pickle.load(f)
+    #         hit = True
+    # except:
+    hit = False
+    sim = Simulation(n_causal_genes=n_causal_genes, p_causal_eqtls=p_causal_eqtls, 
+                     n_train=n_train, bootstrap=bootstrap, hapgen=hapgen, plink=plink)
     try:
         yield sim
     except Exception as e:
