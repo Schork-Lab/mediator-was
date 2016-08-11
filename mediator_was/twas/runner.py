f1ffe676fb038e8726f90630d63643348b3c2561
Kunal Bhutani <kunalbhutani@gmail.com>
2016-08-11 17:12:43 -0400

    Added permutation testing to runner code.

--- mediator_was/twas/runner.py
+++ mediator_was/twas/runner.py
@@ -13,31 +13,49 @@ def associate(gene_dir, study_prefix, out_prefix):
     return
 
 
+def permute(gene_dir, study_prefix,
+                n_permutations, random_state,
+                out_prefix):
+    gene = T.Gene(gene_dir)
+    study = T.Study(study_prefix)
+    association = T.Association(gene, study, associate=False,
+                                permute=(n_permutations, random_state))
+    association.save(out_prefix)
+    return
+
+
 def aggregate(association_dir, prefix=None):
     fns = glob.glob(os.path.join(association_dir, '*.fstats.tsv'))
     print('{} frequentist files found.'.format(len(fns)))
     f_df = pd.concat([pd.read_table(fn, sep='\t')
                      for fn in fns if fn.find('aggregated') == -1])
-    f_df.to_csv(".".join([prefix, 'aggregated.fstats.tsv']), sep='\t', index=False)
+    f_df.to_csv(".".join([prefix, 'aggregated.fstats.tsv']),
+                sep='\t', index=False)
     fns = glob.glob(os.path.join(association_dir, '*.bstats.tsv'))
 
     def b_reader(fn):
-       # Written because of initially not saving gene name
-       df = pd.read_table(fn, sep='\t')
-       if len(df.columns) == 8:
-          df['gene'] = os.path.basename(fn).split('.')[0].split('_')[1]
-          return df[[df.columns[0], 'gene']+list(df.columns[1:-1])]
-       return df
+        '''
+          Written because of initially not saving gene name
+        '''
+        df = pd.read_table(fn, sep='\t')
+        if len(df.columns) == 8:
+            df['gene'] = os.path.basename(fn).split('.')[0].split('_')[1]
+            return df[[df.columns[0], 'gene'] + list(df.columns[1:-1])]
+        return df
+
     b_df = pd.concat([b_reader(fn)
-                     for fn in fns if fn.find('aggregated') == -1])
-    b_df.to_csv(".".join([prefix, 'aggregated.bstats.tsv']), sep='\t', index=False)
+                      for fn in fns if fn.find('aggregated') == -1])
+    b_df.to_csv(".".join([prefix, 'aggregated.bstats.tsv']),
+                sep='\t', index=False)
     return
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Usage: python runner {associate, aggregate}")
         print("python runner.py associate gene_file study_file out_prefix")
         print("python runner.py aggregate association_dir out_prefix")
+        print("python runner.py permutate ")
     else:
         if sys.argv[1] == "associate":
             print('Associating {} to {}'.format(sys.argv[2], sys.argv[3]))
